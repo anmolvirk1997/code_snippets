2972612cfd93f5581eb2cc008075c660b09c3351
anmolvirk1997 <35028100+anmolvirk1997@users.noreply.github.com>
2023-11-26 17:41:49 +0530

    captive.py

--- captive.py
+++ captive.py
@@ -3,11 +3,21 @@ import network
 import ubinascii as binascii
 import uselect as select
 import utime as time
+import uerrno
+import uio
+import uselect as select
+import usocket as socket
+import uos
 
 from captive_dns import DNSServer
 from captive_http import HTTPServer
 from credentials import Creds
+import Server
 
+
+
+from collections import namedtuple
+from credentials import Creds
 
 class CaptivePortal:
     AP_IP = "192.168.4.1"
@@ -166,3 +176,404 @@ class CaptivePortal:
         self.sta_if.active(False)
         if not self.try_connect_from_file():
             self.captive_portal()
+
+WriteConn = namedtuple("WriteConn", ["body", "buff", "buffmv", "write_range"])
+ReqInfo = namedtuple("ReqInfo", ["type", "path", "params", "host"])
+
+
+def unquote(string):
+    """stripped down implementation of urllib.parse unquote_to_bytes"""
+
+    if not string:
+        return b''
+
+    if isinstance(string, str):
+        string = string.encode('utf-8')
+    string = string.replace(b'+', b' ')
+
+    # split into substrings on each escape character
+    bits = string.split(b'%')
+    if len(bits) == 1:
+        return string  # there was no escape character
+    
+    res = [bits[0]]  # everything before the first escape character
+
+    # for each escape character, get the next two digits and convert to 
+    for item in bits[1:]:
+        code = item[:2]
+        char = bytes([int(code, 16)])  # convert to utf-8-encoded byte
+        res.append(char)  # append the converted character
+        res.append(item[2:])  # append anything else that occurred before the next escape character
+    
+    return b''.join(res)
+
+
+class HTTPServer(Server):
+    def __init__(self, poller, local_ip):
+        super().__init__(poller, 80, socket.SOCK_STREAM, "HTTP Server")
+        if type(local_ip) is bytes:
+            self.local_ip = local_ip
+        else:
+            self.local_ip = local_ip.encode()
+        self.request = dict()
+        self.conns = dict()
+        self.routes = {b"/": b"./index.html", b"/login": self.login}
+
+        self.ssid = None
+
+        # queue up to 5 connection requests before refusing
+        self.sock.listen(5)
+        self.sock.setblocking(False)
+
+    def set_ip(self, new_ip, new_ssid):
+        """update settings after connected to local WiFi"""
+
+        self.local_ip = new_ip.encode()
+        self.ssid = new_ssid
+        self.routes = {b"/": self.connected}
+
+    @micropython.native
+    def handle(self, sock, event, others):
+        if sock is self.sock:
+            # client connecting on port 80, so spawn off a new
+            # socket to handle this connection
+            print("- Accepting new HTTP connection")
+            self.accept(sock)
+        elif event & select.POLLIN:
+            # socket has data to read in
+            print("- Reading incoming HTTP data")
+            self.read(sock)
+        elif event & select.POLLOUT:
+            # existing connection has space to send more data
+            print("- Sending outgoing HTTP data")
+            self.write_to(sock)
+
+    def accept(self, server_sock):
+        """accept a new client request socket and register it for polling"""
+
+        try:
+            client_sock, addr = server_sock.accept()
+        except OSError as e:
+            if e.args[0] == uerrno.EAGAIN:
+                return
+
+        client_sock.setblocking(False)
+        client_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.poller.register(client_sock, select.POLLIN)
+
+    def parse_request(self, req):
+        """parse a raw HTTP request to get items of interest"""
+
+        req_lines = req.split(b"\r\n")
+        req_type, full_path, http_ver = req_lines[0].split(b" ")
+        path = full_path.split(b"?")
+        base_path = path[0]
+        query = path[1] if len(path) > 1 else None
+        query_params = (
+            {
+                key: val
+                for key, val in [param.split(b"=") for param in query.split(b"&")]
+            }
+            if query
+            else {}
+        )
+        host = [line.split(b": ")[1] for line in req_lines if b"Host:" in line][0]
+
+        return ReqInfo(req_type, base_path, query_params, host)
+
+    def login(self, params):
+        ssid = unquote(params.get(b"ssid", None))
+        password = unquote(params.get(b"password", None))
+
+        # Write out credentials
+        Creds(ssid=ssid, password=password).write()
+
+        headers = (
+            b"HTTP/1.1 307 Temporary Redirect\r\n"
+            b"Location: http://{:s}\r\n".format(self.local_ip)
+        )
+
+        return b"", headers
+
+    def connected(self, params):
+        headers = b"HTTP/1.1 200 OK\r\n"
+        body = open("./connected.html", "rb").read() % (self.ssid, self.local_ip)
+        return body, headers
+
+    def get_response(self, req):
+        """generate a response body and headers, given a route"""
+
+        headers = b"HTTP/1.1 200 OK\r\n"
+        route = self.routes.get(req.path, None)
+
+        if type(route) is bytes:
+            # expect a filename, so return contents of file
+            return open(route, "rb"), headers
+
+        if callable(route):
+            # call a function, which may or may not return a response
+            response = route(req.params)
+            body = response[0] or b""
+            headers = response[1] or headers
+            return uio.BytesIO(body), headers
+
+        headers = b"HTTP/1.1 404 Not Found\r\n"
+        return uio.BytesIO(b""), headers
+
+    def is_valid_req(self, req):
+        if req.host != self.local_ip:
+            # force a redirect to the MCU's IP address
+            return False
+        # redirect if we don't have a route for the requested path
+        return req.path in self.routes
+
+    def read(self, s):
+        """read in client request from socket"""
+
+        data = s.read()
+        if not data:
+            # no data in the TCP stream, so close the socket
+            self.close(s)
+            return
+
+        # add new data to the full request
+        sid = id(s)
+        self.request[sid] = self.request.get(sid, b"") + data
+
+        # check if additional data expected
+        if data[-4:] != b"\r\n\r\n":
+            # HTTP request is not finished if no blank line at the end
+            # wait for next read event on this socket instead
+            return
+
+        # get the completed request
+        req = self.parse_request(self.request.pop(sid))
+
+        if not self.is_valid_req(req):
+            headers = (
+                b"HTTP/1.1 307 Temporary Redirect\r\n"
+                b"Location: http://{:s}/\r\n".format(self.local_ip)
+            )
+            body = uio.BytesIO(b"")
+            self.prepare_write(s, body, headers)
+            return
+
+        # by this point, we know the request has the correct
+        # host and a valid route
+        body, headers = self.get_response(req)
+        self.prepare_write(s, body, headers)
+
+    def prepare_write(self, s, body, headers):
+        # add newline to headers to signify transition to body
+        headers += "\r\n"
+        # TCP/IP MSS is 536 bytes, so create buffer of this size and
+        # initially populate with header data
+        buff = bytearray(headers + "\x00" * (536 - len(headers)))
+        # use memoryview to read directly into the buffer without copying
+        buffmv = memoryview(buff)
+        # start reading body data into the memoryview starting after
+        # the headers, and writing at most the remaining space of the buffer
+        # return the number of bytes written into the memoryview from the body
+        bw = body.readinto(buffmv[len(headers) :], 536 - len(headers))
+        # save place for next write event
+        c = WriteConn(body, buff, buffmv, [0, len(headers) + bw])
+        self.conns[id(s)] = c
+        # let the poller know we want to know when it's OK to write
+        self.poller.modify(s, select.POLLOUT)
+
+    def write_to(self, sock):
+        """write the next message to an open socket"""
+
+        # get the data that needs to be written to this socket
+        c = self.conns[id(sock)]
+        if c:
+            # write next 536 bytes (max) into the socket
+            try:
+                bytes_written = sock.write(c.buffmv[c.write_range[0] : c.write_range[1]])
+            except OSError:
+                print('cannot write to a closed socket')
+                return
+            if not bytes_written or c.write_range[1] < 536:
+                # either we wrote no bytes, or we wrote < TCP MSS of bytes
+                # so we're done with this connection
+                self.close(sock)
+            else:
+                # more to write, so read the next portion of the data into
+                # the memoryview for the next send event
+                self.buff_advance(c, bytes_written)
+
+    def buff_advance(self, c, bytes_written):
+        """advance the writer buffer for this connection to next outgoing bytes"""
+
+        if bytes_written == c.write_range[1] - c.write_range[0]:
+            # wrote all the bytes we had buffered into the memoryview
+            # set next write start on the memoryview to the beginning
+            c.write_range[0] = 0
+            # set next write end on the memoryview to length of bytes
+            # read in from remainder of the body, up to TCP MSS
+            c.write_range[1] = c.body.readinto(c.buff, 536)
+        else:
+            # didn't read in all the bytes that were in the memoryview
+            # so just set next write start to where we ended the write
+            c.write_range[0] += bytes_written
+
+    def close(self, s):
+        """close the socket, unregister from poller, and delete connection"""
+
+        s.close()
+        self.poller.unregister(s)
+        sid = id(s)
+        if sid in self.request:
+            del self.request[sid]
+        if sid in self.conns:
+            del self.conns[sid]
+        gc.collect()
+
+
+
+class DNSQuery:
+    def __init__(self, data):
+        self.data = data
+        self.domain = ""
+        # header is bytes 0-11, so question starts on byte 12
+        head = 12
+        # length of this label defined in first byte
+        length = data[head]
+        while length != 0:
+            label = head + 1
+            # add the label to the requested domain and insert a dot after
+            self.domain += data[label : label + length].decode("utf-8") + "."
+            # check if there is another label after this one
+            head += length + 1
+            length = data[head]
+
+    def answer(self, ip_addr):
+        # ** create the answer header **
+        # copy the ID from incoming request
+        packet = self.data[:2]
+        # set response flags (assume RD=1 from request)
+        packet += b"\x81\x80"
+        # copy over QDCOUNT and set ANCOUNT equal
+        packet += self.data[4:6] + self.data[4:6]
+        # set NSCOUNT and ARCOUNT to 0
+        packet += b"\x00\x00\x00\x00"
+
+        # ** create the answer body **
+        # respond with original domain name question
+        packet += self.data[12:]
+        # pointer back to domain name (at byte 12)
+        packet += b"\xC0\x0C"
+        # set TYPE and CLASS (A record and IN class)
+        packet += b"\x00\x01\x00\x01"
+        # set TTL to 60sec
+        packet += b"\x00\x00\x00\x3C"
+        # set response length to 4 bytes (to hold one IPv4 address)
+        packet += b"\x00\x04"
+        # now actually send the IP address as 4 bytes (without the "."s)
+        packet += bytes(map(int, ip_addr.split(".")))
+
+        gc.collect()
+
+        return packet
+
+
+class DNSServer(Server):
+    def __init__(self, poller, ip_addr):
+        super().__init__(poller, 53, socket.SOCK_DGRAM, "DNS Server")
+        self.ip_addr = ip_addr
+
+    def handle(self, sock, event, others):
+        # server doesn't spawn other sockets, so only respond to its own socket
+        if sock is not self.sock:
+            return
+
+        # check the DNS question, and respond with an answer
+        try:
+            data, sender = sock.recvfrom(1024)
+            request = DNSQuery(data)
+
+            print("Sending {:s} -> {:s}".format(request.domain, self.ip_addr))
+            sock.sendto(request.answer(self.ip_addr), sender)
+
+            # help MicroPython with memory management
+            del request
+            gc.collect()
+        except Exception as e:
+            print("DNS server exception:", e)
+
+
+class Server:
+    def __init__(self, poller, port, sock_type, name):
+        self.name = name
+        # create socket with correct type: stream (TCP) or datagram (UDP)
+        self.sock = socket.socket(socket.AF_INET, sock_type)
+
+        # register to get event updates for this socket
+        self.poller = poller
+        self.poller.register(self.sock, select.POLLIN)
+
+        addr = socket.getaddrinfo("0.0.0.0", port)[0][-1]
+        # allow new requests while still sending last response
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind(addr)
+
+        print(self.name, "listening on", addr)
+
+    def stop(self, poller):
+        poller.unregister(self.sock)
+        self.sock.close()
+        print(self.name, "stopped")
+
+class Creds:
+
+    CRED_FILE = "./wifi.creds"
+
+    def __init__(self, ssid=None, password=None):
+        self.ssid = ssid
+        self.password = password
+
+    def write(self):
+        """Write credentials to CRED_FILE if valid input found."""
+        if self.is_valid():
+            with open(self.CRED_FILE, "wb") as f:
+                f.write(b",".join([self.ssid, self.password]))
+            print("Wrote credentials to {:s}".format(self.CRED_FILE))
+
+    def load(self):
+
+        try:
+            with open(self.CRED_FILE, "rb") as f:
+                contents = f.read().split(b",")
+            print("Loaded WiFi credentials from {:s}".format(self.CRED_FILE))
+            if len(contents) == 2:
+                self.ssid, self.password = contents
+
+            if not self.is_valid():
+                self.remove()
+        except OSError:
+            pass
+
+        return self
+
+    def remove(self):
+        """
+        1. Delete credentials file from disk.
+        2. Set ssid and password to None
+        """
+        # print("Attempting to remove {}".format(self.CRED_FILE))
+        try:
+            uos.remove(self.CRED_FILE)
+        except OSError:
+            pass
+
+        self.ssid = self.password = None
+
+    def is_valid(self):
+        # Ensure the credentials are entered as bytes
+        if not isinstance(self.ssid, bytes):
+            return False
+        if not isinstance(self.password, bytes):
+            return False
+
+        # Ensure credentials are not None or empty
+        return all((self.ssid, self.password))
